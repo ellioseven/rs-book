e25590cfc6f63a8923917d9150ece364f92674b6
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-01-09 10:31:35 +1100

    Add search

--- grep/src/lib.rs
+++ grep/src/lib.rs
@@ -3,7 +3,12 @@ use std::error::Error;
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
-    println!("{}", contents);
+    let results = search(&config.query, &contents);
+
+    for line in results {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
@@ -23,5 +28,29 @@ impl Config {
         let config = Config { query, filename };
 
         Ok(config)
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "a";
+        let contents = "foo\nbar\nbaz";
+        assert_eq!(vec!["bar", "baz"], search(query, contents));
     }
 }
