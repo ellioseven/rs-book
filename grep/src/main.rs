109a6eedc2c5f311c1331a89df48dd17a30e70a7
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-01-09 11:02:34 +1100

    Use std err

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|e| {
-        println!("config error: {}", e);
+        eprintln!("config error: {}", e);
         process::exit(1);
     });
 
     if let Err(e) = grep::run(&config) {
-        println!("error: {}", e);
+        eprintln!("error: {}", e);
         process::exit(1);
     }
 }
